f7085fbd7b7a30f61100adc9dc4472a88edde891
Esteban Dalel R <esteban@astronuts.io>
2024-09-24 17:24:46 +0200

    Improve playground

---
+++ app/playground/output_list.tsx
@@ -0,0 +1,83 @@
+import { MemoizedReactMarkdown } from '@/components/markdown'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from '@/components/ui/tooltip'
+const OutputList = ({
+  completions
+}: {
+  completions: {
+    content: string
+    startTimestamp: number
+    endTimestamp: number
+    input: string
+  }[]
+}) => {
+  return (
+    <div className="flex flex-col-reverse gap-2">
+      {completions.map((completion, index) => (
+        <Card key={index} className="border border-gray-300 rounded p-2">
+          <CardHeader>
+            <CardTitle className="text-xl font-bold">
+              Completion {index + 1}
+            </CardTitle>
+            <CardDescription>
+              <MemoizedReactMarkdown>{completion.input}</MemoizedReactMarkdown>
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <MemoizedReactMarkdown>{completion.content}</MemoizedReactMarkdown>
+          </CardContent>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <CardFooter>
+                <p>
+                  Time taken:{' '}
+                  {completion.endTimestamp - completion.startTimestamp}
+                  ms
+                </p>
+              </CardFooter>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>
+                Start:{' '}
+                {new Intl.DateTimeFormat('en-US', {
+                  year: 'numeric',
+                  month: 'long',
+                  day: 'numeric',
+                  hour: 'numeric',
+                  minute: 'numeric',
+                  second: 'numeric',
+                  timeZoneName: 'short'
+                }).format(new Date(completion.endTimestamp))}{' '}
+              </p>
+              <p>
+                End:{' '}
+                {new Intl.DateTimeFormat('en-US', {
+                  year: 'numeric',
+                  month: 'long',
+                  day: 'numeric',
+                  hour: 'numeric',
+                  minute: 'numeric',
+                  second: 'numeric',
+                  timeZoneName: 'short'
+                }).format(new Date(completion.startTimestamp))}{' '}
+              </p>
+            </TooltipContent>
+          </Tooltip>
+        </Card>
+      ))}
+    </div>
+  )
+}
+export default OutputList
--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,32 +1,62 @@
 'use client'
 import { Textarea } from '@/components/ui/textarea'
-
+import { useCompletion } from 'ai/react'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import SettingsForm from './settings_form'
+import { useEffect, useState } from 'react'
+import OutputList from './output_list'
 
 export default function Playground() {
+  const [completions, setCompletions] = useState<
+    {
+      content: string
+      startTimestamp: number
+      endTimestamp: number
+      input: string
+    }[]
+  >([])
+  const [startTimestamp, setStartTimestamp] = useState<number>(Date.now())
+  const [askedInput, setAskedInput] = useState<string>('')
   const formSchema = z.object({
     model: z.enum(['gpt2', 'gpt3', 'gpt4']),
     temperature: z.number(),
-    maxTokens: z.number(),
-    topP: z.number(),
-    stream: z.boolean()
+    maxTokens: z.number()
   })
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       model: 'gpt2',
       temperature: 0.7,
-      maxTokens: 500,
-      topP: 0.8,
-      stream: false
+      maxTokens: 500
     }
   })
   const onSubmit = (data: z.infer<typeof formSchema>) => {
     console.log(data)
   }
+  const { completion, input, setInput, handleSubmit, isLoading } =
+    useCompletion({
+      body: {
+        data: form.getValues()
+      },
+      api: '/api/playground'
+    })
+  useEffect(() => {
+    if (isLoading) {
+      setStartTimestamp(Date.now())
+    }
+    if (completion && !isLoading) {
+      const newCompletion = {
+        content: completion,
+        startTimestamp,
+        endTimestamp: Date.now(),
+        input: askedInput
+      }
+      console.log('New completion', newCompletion)
+      setCompletions([...completions, newCompletion])
+    }
+  }, [isLoading])
 
   return (
     <main className="flex flex-col p-4 size-full">
@@ -40,13 +70,24 @@ export default function Playground() {
         <div className="flex flex-col p-4 w-2/3">
           <h2 className="text-2xl font-bold">Input</h2>
           <Textarea
-            className="p-2 border border-gray-300 rounded"
+            className="p-2 border border-gray-300 rounded bg-primary-foreground text-primary-background"
             rows={10}
             placeholder="Enter your text here"
             style={{ resize: 'none' }}
+            onChange={(event: { target: { value: any } }) => {
+              setInput(event.target.value)
+            }}
+            value={input}
+            onKeyDown={async (event: { key: string }) => {
+              if (event.key === 'Enter') {
+                setAskedInput(input)
+                handleSubmit()
+                setInput('')
+              }
+            }}
           />
           <h2 className="text-2xl font-bold">Output</h2>
-          <Textarea readOnly style={{ resize: 'none' }} />
+          <OutputList completions={completions} />
         </div>
       </div>
     </main>
